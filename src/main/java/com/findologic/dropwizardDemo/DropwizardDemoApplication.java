32a6794f51c4fd56944167f05dee0336baf30dbd
Chris Ortner <chris@codexfons.com>
2017-06-05 11:45:48 +0200

    Enable metrics with Graphite
    1. Add Graphite metrics reporter dependency.
    2. Make connection data configurable.
    3. Enable the Graphite reporter in DropwizardDemoApplication.

--- src/main/java/com/findologic/dropwizardDemo/DropwizardDemoApplication.java
+++ src/main/java/com/findologic/dropwizardDemo/DropwizardDemoApplication.java
@@ -1,8 +1,14 @@
 package com.findologic.dropwizardDemo;
 
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.graphite.Graphite;
+import com.codahale.metrics.graphite.GraphiteReporter;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
 
 public class DropwizardDemoApplication extends Application<DropwizardDemoConfiguration> {
 
@@ -23,6 +29,25 @@ public class DropwizardDemoApplication extends Application<DropwizardDemoConfigu
     @Override
     public void run(final DropwizardDemoConfiguration configuration,
                     final Environment environment) {
-        // TODO application implementation
+        enableGraphiteReports(configuration, environment);
+    }
+
+    private void enableGraphiteReports(final DropwizardDemoConfiguration configuration, final Environment environment) {
+        final Graphite graphiteClient = new Graphite(
+                new InetSocketAddress(configuration.getGraphiteHost(), configuration.getGraphitePort()));
+
+        final GraphiteReporter reporter = GraphiteReporter.forRegistry(environment.metrics())
+                .prefixedWith("dropwizard-demo")
+                        // All rate units should be something per second.
+                .convertRatesTo(TimeUnit.SECONDS)
+                        // We're going fast, so let's measure durations in milliseconds.
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                        // For demo purposes, let's collect all metrics. In production, you might want to use a more specific
+                        // filter to increase performance.
+                .filter(MetricFilter.ALL)
+                .build(graphiteClient);
+
+        // Push metrics to Graphite every ten seconds.
+        reporter.start(10, TimeUnit.SECONDS);
     }
 }
--- src/main/java/com/findologic/dropwizardDemo/DropwizardDemoConfiguration.java
+++ src/main/java/com/findologic/dropwizardDemo/DropwizardDemoConfiguration.java
@@ -1,6 +1,32 @@
 package com.findologic.dropwizardDemo;
 
 import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
 
 public class DropwizardDemoConfiguration extends Configuration {
+    @NotEmpty
+    private String graphiteHost;
+
+    private int graphitePort;
+
+    @JsonProperty
+    public String getGraphiteHost() {
+        return graphiteHost;
+    }
+
+    @JsonProperty
+    public void setGraphiteHost(String graphiteHost) {
+        this.graphiteHost = graphiteHost;
+    }
+
+    @JsonProperty
+    public int getGraphitePort() {
+        return graphitePort;
+    }
+
+    @JsonProperty
+    public void setGraphitePort(int graphitePort) {
+        this.graphitePort = graphitePort;
+    }
 }
